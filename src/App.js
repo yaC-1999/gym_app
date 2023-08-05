5ad6f64451db0a5430537ce6c5c33a46b54b4351
yasamin <yasamin.sahragard@gmail.com>
2023-08-05 10:26:55 +0330

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width:{xl:'1488px' }}} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
