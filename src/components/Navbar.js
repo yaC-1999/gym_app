420fef4234600bafe6f9ee42100aebe469b73750
yasamin <yasamin.sahragard@gmail.com>
2023-08-05 11:00:38 +0330

    navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Stack from "@mui/material/Stack";
+import { Link } from "@mui/material";
+import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{ gap: { sm: "122px", xs: "40px" }, mt: { sm: "32px", xs: "20px" } , justifyContent:'none' }}
+      px="20px"
+    >
+      <Link>
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: "48px", height: "48px", margin: "0 20px" }}
+        />
+      </Link>
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "black",
+            borderBottom: "3px solid red",
+          }}
+        >
+          Home
+        </Link>
+        <Link
+          to="#exercises"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          Exercises
+        </Link>
+      </Stack>
+    </Stack>
+  );
+};
 
-export default Navbar
+export default Navbar;
