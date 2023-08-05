f825abaf2fe40f87b02dc9a335b724e5efabd3f7
yasamin <yasamin.sahragard@gmail.com>
2023-08-05 10:23:23 +0330

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Box } from "@mui/material";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import ExerciseDetail from "./components/ExerciseDetail";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
