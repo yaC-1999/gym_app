a68bf3592b820816af8ebb310dd080a5577d42ca
yasamin <yasamin.sahragard@gmail.com>
2023-08-20 22:54:19 +0330

    fetch data

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,15 +1,25 @@
-
 export const exercisesOptions = {
   method: "GET",
-//   url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
   headers: {
-    'X-RapidAPI-Key': '668e0e5d03msh61fd2d71ae93a95p160ae8jsn7a2a0dbed022',
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    "X-RapidAPI-Key": "668e0e5d03msh61fd2d71ae93a95p160ae8jsn7a2a0dbed022",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "668e0e5d03msh61fd2d71ae93a95p160ae8jsn7a2a0dbed022",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
 
 export const fetchData = async (url, options) => {
-  const response = await fetch(url, options);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+  } catch(error) {
+    console.error(error);
+  }
 };
