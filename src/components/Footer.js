0fecd6599ff4cd8c489eb2669ea386718c96f5eb
yasamin <yasamin.sahragard@gmail.com>
2023-08-20 23:04:24 +0330

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Grid, Typography } from "@mui/material";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Grid m={1} display="flex" justifyContent="center" alignItems="center">
+      <Typography variant="subtitle2">Made by <span style={{color:'red'}} >Yasamin</span> :)</Typography>
+    </Grid>
+  );
+};
 
-export default Footer
+export default Footer;
